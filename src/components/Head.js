02fd816f3eaf2459b2cb3c5313325ad5303fae6a
williamwmarx <william@marx.design>
2020-08-10 02:39:16 -0500

    minor changes

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,6 +3,8 @@ import Helmet from "react-helmet";
 
 export default function Head(props) {
     return (
-       <Helmet title={props.title} defer={false} />
+      <Helmet title={props.title} defer={false}>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Helmet>
     );
 }
